37557d0fef180d25849bc87e978b3172fd4fa7b8
lottola <lottolaaa@gmail.com>
2025-11-11 09:13:53 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,10 @@
 import HomeNavbar from '@/components/HomeNavbar.vue'
 import AdminNavbar from '@/components/AdminNavbar.vue'
 import { useRoute } from 'vue-router'
-import { useDark, useToggle } from '@vueuse/core'
 const route = useRoute()
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
 </script>
 <template>
   <div>
-    <p>{{ isDark ? 'ON' : 'OFF' }}</p>
-    <button @click="toggleDark()">Toggle</button>
-    <div class="p-4 bg-white text-black dark:bg-gray-900 dark:text-white">Probe block</div>
     <AdminNavbar
       v-if="
         route.name == 'AdminDashboard' ||
