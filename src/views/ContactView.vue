5a553a27639ab46685172de43924e2c015347f68
lottola <lottolaaa@gmail.com>
2025-11-25 11:17:00 +0700

    add feature

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,3 +1,53 @@
+<script setup>
+import { ref, reactive } from 'vue'
+import axios from 'axios'
+const message = ref('')
+const firstName = ref('')
+const lastName = ref('')
+const email = ref('')
+const messageState = reactive({
+  isSuccess: false,
+  message: '',
+  loading: false,
+})
+const handleSubmit = async () => {
+  messageState.loading = true
+  let name = firstName.value
+  if (lastName) {
+    name = firstName.value + ' ' + lastName.value
+  }
+  const payload = {
+    to: 'support@eduvision.live',
+    subject: 'Support Ticket',
+    message: `Sender Name: ${name}\nEmail: ${email.value}\nMessage: ${message.value}`,
+  }
+  try {
+    const resp = await axios.post('https://gmail.corevibe.tech', payload, {
+      'Content-Type': 'application/json',
+    })
+    if (resp.status === 200) {
+      messageState.loading = false
+      messageState.message = resp.data.message
+      messageState.isSuccess = true
+      message.value = ''
+      email.value = ''
+      firstName.value = ''
+      lastName.value = ''
+      setTimeout(() => {
+        messageState.message = ''
+      }, 5000)
+    }
+  } catch (err) {
+    console.log(err)
+    messageState.loading = false
+    messageState.isSuccess = false
+    messageState.message = 'Something went wrong please try again later.'
+    setTimeout(() => {
+      messageState.message = ''
+    }, 5000)
+  }
+}
+</script>
 <template>
   <div class="grid grid-cols-3 grid-rows-2 mt-10">
     <div class="col-start-1 my-auto">
@@ -13,24 +63,53 @@
       </p>
     </div>
     <div class="col-start-2 col-span-2 w-200 mx-auto">
-      <form class="flex flex-col" action="">
+      <p
+        v-if="messageState.message && messageState.isSuccess"
+        class="text-center w-fit mx-auto py-3 rounded-lg mb-4 text-xl px-10 bg-green-400 text-white font-bold"
+      >
+        {{ messageState.message }}
+      </p>
+      <p
+        v-if="messageState.message && !messageState.isSuccess"
+        class="text-center w-fit mx-auto py-3 rounded-lg mb-4 text-xl px-10 bg-red-400 text-white font-bold"
+      >
+        {{ messageState.message }}
+      </p>
+      <form class="flex flex-col" @submit.prevent="handleSubmit">
         <div class="grid grid-cols-2 w-full gap-3 mb-2">
           <div class="col-start-1 w-full">
             <label for="firstname " class="block mb-2" name="firstname"
               >First Name <span class="text-red-500">*</span></label
             >
-            <input type="text" id="firstname" class="input input-primary w-full" required />
+            <input
+              v-model="firstName"
+              type="text"
+              id="firstname"
+              class="input input-primary w-full"
+              required
+            />
           </div>
           <div class="col-start-2 w-full">
             <label for="lastname" class="block mb-2" name="lastname">Last Name</label>
-            <input type="text" id="lastname" class="w-full input input-primary" />
+            <input
+              v-model="lastName"
+              type="text"
+              id="lastname"
+              class="w-full input input-primary"
+            />
           </div>
         </div>
         <div class="mb-2">
           <label for="email" class="block mb-2" name="email"
             >Email <span class="text-red-500">*</span></label
           >
-          <input type="email" id="email" class="w-full input input-primary" required />
+          <input
+            type="email"
+            v-model="email"
+            id="email"
+            class="w-full input input-primary"
+            required
+          />
         </div>
         <div class="mb-5">
           <label for="message" class="block mb-2" name="message"
@@ -38,6 +117,7 @@
           >
           <input
             type="message"
+            v-model="message"
             id="message"
             class="w-full h-40 input input-primary"
             required
@@ -45,7 +125,8 @@
             max="1000"
           />
         </div>
-        <button type="submit" class="button mx-auto"><span>Send</span></button>
+        <span v-if="messageState.loading" class="loading loading-dots loading-lg mx-auto"></span>
+        <button v-else type="submit" class="button mx-auto"><span>Send</span></button>
       </form>
     </div>
     <div class="flex gap-10 items-center col-span-3 row-start-2 mx-auto">
